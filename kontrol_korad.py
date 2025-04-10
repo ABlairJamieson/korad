9aff243e9a2e4fe8777aca9a2c9f561173c3e980
Blair Jamieson <bl.jamieson@uwinnipeg.ca>
2025-04-10 15:18:58 -0500

    update for 4-channel Korad

--- kontrol_korad.py
+++ kontrol_korad.py
@@ -3,131 +3,140 @@
 import serial
 import time
 
+# /dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_002800D40252-if00
 class KoradPowerSupply:
-    def __init__(self, id='01', port: str = "/dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_002800D40252-if00", baudrate: int = 11520, timeout: float = 1):
-        self.id = id
+    '''
+    Class to control a single Korad KC3405 four channel power supply.
+    Note that it allows each channel to be querried if run from different threads, hence the threadlock checks
+    in the readbacks of the voltages and currents.  It assumes that only one voltage change, or current will be
+    done at a time.
+
+    The class is initialized with the serial device (assuming connected via USB serial device in linux).
+    '''
+    
+    def __init__(self, port: str = "/dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_000016E90000-if00", baudrate: int = 11520, timeout: float = 1):
         self.ser = serial.Serial(port, baudrate=baudrate, bytesize=serial.EIGHTBITS, parity=serial.PARITY_NONE,
                         stopbits=serial.STOPBITS_ONE, timeout=timeout, rtscts=False, xonxoff=False, dsrdtr=False)
+        self.threadlock = False
         time.sleep(1)
     
     def send_command(self, command: str):
-        """Send a command to the power supply."""
+        """Send a command to the power supply for the specified channel."""
         self.ser.write(f'{command}\r\n'.encode('utf-8'))
         time.sleep(0.1)
     
-    def beep_on(self):
-        """Beep the power supply."""
-        self.send_command('BEEP'+self.id+':1')
-    
-    def beep_off(self):
-        """Stop the beep of the power supply."""
-        self.send_command('BEEP'+self.id+':0')
-    
-    def get_id(self):
-        """Get the ID of the power supply."""
-        return self.id
-
     def get_serialno(self):
         """Get the Serial number of the power supply."""
         self.send_command('*IDN?')
         response = self.read_response()
         return response
 
-    def get_voltage(self) -> float:
+    def get_voltage(self, channel: str = '1') -> float:
         """Retrieve the output voltage from the power supply."""
-        self.send_command('VOUT'+self.id+'?')
-        response = self.read_response()
+        while self.threadlock:
+            time.sleep(0.1)
+        self.threadlock=True
+        try:
+            self.send_command('VOUT'+channel+'?')
+            response = self.read_response()
+        except:
+            pass
+        self.threadlock=False
         return float(response) if response else 0.0
     
-    def get_current(self) -> float:
+    def get_current(self, channel: str = '1') -> float:
         """Retrieve the output current from the power supply."""
-        self.send_command('IOUT'+self.id+'?')
-        response = self.read_response()
+        while self.threadlock:
+            time.sleep(0.1)
+        self.threadlock=True
+        try:
+            self.send_command('IOUT'+channel+'?')
+            response = self.read_response()
+        except:
+            pass
+        self.threadlock=False
         return float(response) if response else 0.0
     
-    def set_voltage(self, voltage: float):
+    def set_voltage(self, voltage: float, channel: str = '1'):
         """Set the output voltage of the power supply."""
-        command = f'VSET{self.id}:{voltage:.3f}'  
+        command = f'VSET{channel}:{voltage:.3f}'  
         print(command)
         self.send_command(command)
         time.sleep(0.1)
     
-    def set_current(self, current: float):
+    def set_current(self, current: float, channel: str='1'):
         """Set the output current of the power supply."""
-        self.send_command(f'ISET{self.id}:{current:.3f}')
+        self.send_command(f'ISET{channel}:{current:.3f}')
         time.sleep(0.1)
     
-    def get_output_onoff(self):
-        """Check if the power supply output is on."""
-        self.send_command('OUT'+self.id+'?')
-        response = self.read_response()
-        return bool(int(response)) if response else False
     
-    def turn_output_on(self):
+    def turn_output_on(self, channel:str='1'):
         """Turn the power supply output on."""
-        self.send_command('OUT'+self.id+':1')
+        self.send_command('OUT'+channel+':1')
+        time.sleep(1)
         if self.get_output_onoff() == False:
             print("Failed to turn output on")
         else:
             print("Output turned on")
     
-    def turn_output_off(self):
+    def turn_output_off(self, channel:str='1'):
         """Turn the power supply output off."""
-        self.send_command('OUT'+self.id+':0')
+        self.send_command('OUT'+channel+':0')
+        time.sleep(0.1)
+        
         if self.get_output_onoff() == False:
             print("Output turned off")
         else:
             print("Failed to turn off output")
-
+            
     def read_response(self):    
         """Read the response from the power supply."""
         response = self.ser.readline().decode('utf-8').strip()
         return response
     
-    def read_response_status(self):
-        """Read the response from the power supply."""
-        """ response is b'\x17\n' use int.from_bytes(response, byteorder='big')"""
-        response = self.ser.read(2)
-        return int.from_bytes(response, byteorder='big') if response else None
+
+
+    def get_output_onoff(self, channel:str='1'):
+        """Check if the power supply output is on."""
+        setV = self.get_voltage_set(channel)
+        readV = self.get_voltage(channel)
+
+        if abs(setV-readV) > 0.05:
+            return False
+        else:
+            return True
+        
     
-    def report_status(self):
-        """Report the status of the power supply."""
-        self.send_command('STATUS'+self.id+'?')
-        response = self.read_response_status()
-        if response:
-            print(f"Status: {response}")  
-            print(f"CC/CV: {response & 1}")  # bit 0
-            print(f"Output: {(response & 2) >> 1}") # bit 1
-            print(f"V/C priority: {(response & 4) >> 2}") # bit 2
-            print(f"Buzzer: {(response & 16) >> 4}") # bit 4
-            print(f"Lock: {(response & 32) >> 5}") # bit 5
-            print(f"OVP status: {(response & 64) >> 6}") # bit 6
-            print(f"OCP status: {(response & 128) >> 7}") # bit 7
-            return response
-        return None
-    
-    def get_voltage_set(self) -> float:
+    def get_voltage_set(self, channel='1') -> float:
         """Retrieve the output voltage from the power supply."""
-        self.send_command('VSET'+self.id+'?')
+        while self.threadlock:
+            time.sleep(0.1)
+        self.threadlock = True
+        self.send_command('VSET'+channel+'?')
         response = self.read_response()
+        self.threadlock = False
         return float(response) if response else 0.0
     
-    def get_current_set(self) -> float:
+    def get_current_set(self, channel:str='1') -> float:
         """Retrieve the output current from the power supply."""
-        self.send_command('ISET'+self.id+'?')
+        while self.threadlock:
+            time.sleep(0.1)
+        self.threadlock = True
+        self.send_command('ISET'+channel+'?')
         response = self.read_response()
+        self.threadlock = False
         return float(response) if response else 0.0
 
-    def report_IV(self):
+    def report_IV(self,channel:str='1'):
         """Report the voltage and current set and readback
           of the power supply."""
-        setV = self.get_voltage_set()
-        setI = self.get_current_set()
-        readV = self.get_voltage()
-        readI = self.get_current()
-        onoff = self.get_output_onoff()
+        setV = self.get_voltage_set(channel)
+        setI = self.get_current_set(channel)
+        readV = self.get_voltage(channel)
+        readI = self.get_current(channel)
+        onoff = self.get_output_onoff(channel)
         # print as one line set voltage, current and on/off status
-        print(f"Set V: {setV:.3f}V, Set I: {setI:.3f}A, Read V: {readV:.3f}V, Read I: {readI:.3f}A, Output: {onoff}")   
+        print(f"Ch: {channel} Set V: {setV:.3f}V, Set I: {setI:.3f}A, Read V: {readV:.3f}V, Read I: {readI:.3f}A, Output: {onoff}")   
 
     def disconnect(self):
         """Disconnect the power supply."""
@@ -136,34 +145,24 @@ class KoradPowerSupply:
             
 # Example usage
 if __name__ == "__main__":
-    port = "/dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_002800D40252-if00"
-    psu = KoradPowerSupply(port=port, id='01')
+    port = "/dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_000016E90000-if00"
+    #"/dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_002800D40252-if00"
+    channel='1' # for example test just this channel for now
+    psu = KoradPowerSupply(port=port)
     print('Serial number: ' + psu.get_serialno())    
-    print('id=' + psu.get_id())
-    psu.beep_on()
-    time.sleep(1)
-    psu.beep_off()
 
-    print(f"Voltage set: {psu.get_voltage_set()}V")
-    print(f"Current set: {psu.get_current_set()}A")
+    print(f"Voltage set: {psu.get_voltage_set(channel)}V")
+    print(f"Current set: {psu.get_current_set(channel)}A")
 
-    psu.set_voltage(5.11)
-    print("Voltage set to 5.11V")
+    psu.set_voltage(5.11,channel)
+    print("Voltage set to 5.11 V")
         
-    psu.set_current(0.05)
-    print("Current set to 0.05A")
-        
-    psu.turn_output_on()
+    psu.set_current(0.1,channel)
+    print("Current set to 0.1 A")
 
-    status = psu.report_status()
-    psu.report_IV()        
+    psu.report_IV(channel)        
 
-
-    psu.turn_output_off()
-
-    status = psu.report_status()
-
-    psu.report_IV()    
+    psu.report_IV(channel)    
     psu.disconnect()
 
 
--- korad_tk_gui.py
+++ korad_tk_gui.py
@@ -7,10 +7,17 @@ import datetime
 import threading
 
 class KonradKontrolWindow(tk.Tk):
-    def __init__(self, port, channel_id, *args, **kwargs):
+    '''
+    Tk window that holds all of the channel widgets.  Each channel has its own frame tk.Frame object (KoradControlWidget)
+    which runs its update in a separate thread.  Since we only want to connect serial port to one KoradPowerSupply the
+    channels share the serial port connection, and some management of locking to not have two threads talking to the PS
+    at once are implemented in kontrol_korad.
+    '''
+    def __init__(self, port, channel_ids, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.port = port
-        self.channel_id = channel_id
+        self.channel_ids = channel_ids
+        self.koradctl = korad.KoradPowerSupply(port=self.port)
         tk.Tk.wm_title(self,"Korad Kontrol")
         #self.geometry("800x200")
         self.create_widgets()
@@ -24,9 +31,10 @@ class KonradKontrolWindow(tk.Tk):
         self.date_time.pack()
         self.update_date_time()
         
-        self.channels = []
-        self.channels.append(KonradChannelWidget(self.port, self.channel_id))
-        self.channels[0].pack(side=tk.TOP)
+        self.channels = {}
+        for chan in self.channel_ids:
+            self.channels[chan] = KonradChannelWidget( self.koradctl, chan )
+            self.channels[chan].pack(side=tk.TOP)
 
         # quit button
         self.quit_button = ttk.Button(self, text="Quit", command=self.quit)
@@ -34,8 +42,7 @@ class KonradKontrolWindow(tk.Tk):
 
     def quit(self):
         print('quitting')
-        for channel in self.channels:
-            channel.korad.disconnect()
+        self.koradctl.disconnect()
         return super().quit()
 
     def update_date_time(self):
@@ -46,22 +53,20 @@ class KonradKontrolWindow(tk.Tk):
         self.after(1000, self.update_date_time) 
 
     def mainloop(self):
-        for channel in self.channels:
-            print('update channel:',channel.channel_id)
-            channel.update()
+        for chan in self.channel_ids:
+            print('update channel:', chan )
+            self.channels[chan].update()
         tk.Tk.update(self)
         tk.Tk.mainloop(self)
 
 class KonradChannelWidget(tk.Frame):
-    def __init__(self, port, channel_id, *args, **kwargs):
+    def __init__(self, koradctl, channel, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.port = port
-        self.channel_id = channel_id
+        self.koradctl = koradctl
+        self.channel = channel
 
         #self.title("Korad Kontrol")
         #self.geometry("600x300")
-
-        self.korad = korad.KoradPowerSupply(port=self.port, id=self.channel_id)
 
         self.create_widgets()
         self['padx'] = 5
@@ -77,7 +82,7 @@ class KonradChannelWidget(tk.Frame):
             The readbacks should update once every 15 seconds 
         '''
         # put channel id label at the top
-        self.channel_id_label = ttk.Label(self, text="Channel "+self.channel_id)
+        self.channel_id_label = ttk.Label(self, text="Channel "+self.channel )
         self.channel_id_label.pack(side=tk.TOP,pady=5)
 
 
@@ -87,7 +92,7 @@ class KonradChannelWidget(tk.Frame):
         self.voltage_frame.pack(side=tk.TOP,pady=5)
         self.voltage_label = ttk.Label(self.voltage_frame, text="V Set (V)")
         self.voltage_entry = ttk.Entry(self.voltage_frame, width=10)
-        voltage = self.korad.get_voltage_set()
+        voltage = self.koradctl.get_voltage_set( self.channel )
         print('voltage:',voltage)
         self.voltage_entry.insert(0,str(voltage))
         self.set_voltage_button = ttk.Button(self.voltage_frame, text="Set V", command=self.set_voltage)
@@ -105,7 +110,7 @@ class KonradChannelWidget(tk.Frame):
         self.current_frame.pack(side=tk.TOP,pady=5)
         self.current_label = ttk.Label(self.current_frame, text="I Set (A):")
         self.current_entry = ttk.Entry(self.current_frame,width=10)
-        current = self.korad.get_current_set()
+        current = self.koradctl.get_current_set( self.channel )
         print('current:',current)
         self.current_entry.insert(0,str(current))
         self.set_current_button = ttk.Button(self.current_frame, text="Set I", command=self.set_current)
@@ -129,24 +134,24 @@ class KonradChannelWidget(tk.Frame):
 
 
     def toggle_output(self):
-        if self.korad.get_output_onoff():
-            self.korad.turn_output_off()
+        if self.koradctl.get_output_onoff( self.channel ):
+            self.koradctl.turn_output_off( self.channel )
         else:
-            self.korad.turn_output_on()
+            self.koradctl.turn_output_on( self.channel )
         self.get_output()
 
     def set_voltage(self):
-        self.korad.set_voltage( float(self.voltage_entry.get()) )
+        self.koradctl.set_voltage( float(self.voltage_entry.get()), self.channel )
 
     def set_current(self):
-        self.korad.set_current( float(self.current_entry.get()) )
+        self.koradctl.set_current( float(self.current_entry.get()), self.channel )
 
     def get_voltage(self):
-        voltage = self.korad.get_voltage()
+        voltage = self.koradctl.get_voltage( self.channel )
         self.voltage_readback.config(text=str(voltage)+' V')
  
     def get_current(self):
-        current = self.korad.get_current()
+        current = self.koradctl.get_current( self.channel )
         self.current_readback.config(text=str(current)+' A')
 
     def get_output(self):
@@ -156,10 +161,10 @@ class KonradChannelWidget(tk.Frame):
         otherwise make it green. 
         '''
         voltage_set = float(self.voltage_entry.get())
-        voltage_read = self.korad.get_voltage()
+        voltage_read = self.koradctl.get_voltage( self.channel )
         current_set = float(self.current_entry.get())
-        current_read = self.korad.get_current()
-        if self.korad.get_output_onoff():
+        current_read = self.koradctl.get_current( self.channel )
+        if self.koradctl.get_output_onoff( self.channel ):
             self.output_button.config(text="Turn OFF")
             self.output_status_label.config(text="Output: ON")
             if abs(voltage_set-voltage_read) > 0.01:
@@ -198,10 +203,10 @@ if __name__ == '__main__':
 
     root.destroy()
 
-    # use argparse to get the port and id from the command line
+    # use argparse to get the port and channel numbers from the command line
     parser = argparse.ArgumentParser(description='Korad Kontrol')
-    parser.add_argument('--port', default='/dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_002800D40252-if00',type=str, help='The serial port to use')
-    parser.add_argument('--id', default='01',type=str, help='The ID of the power supply')
+    parser.add_argument('--port', default='/dev/serial/by-id/usb-Nuvoton_KORAD_USB_Mode_000016E90000-if00',type=str, help='The serial port to use')
+    parser.add_argument('--id', default=['1','2','3','4'],nargs='+', help='List of channels')
     args = parser.parse_args()
 
     app = KonradKontrolWindow( args.port, args.id )
